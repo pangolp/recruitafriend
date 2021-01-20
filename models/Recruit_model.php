126623cbe8517c182164540280fbbdb7fc75042b
Pagani Walter <paganiwalter@gmail.com>
2021-01-19 23:27:26 -0300

    Version 1 of the model

---
+++ models/Recruit_model.php
@@ -0,0 +1,70 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+/**
+ * BlizzCMS
+ *
+ * An Open Source CMS for "World of Warcraft"
+ *
+ * This content is released under the MIT License (MIT)
+ *
+ * Copyright (c) 2017 - 2021, WoW-CMS
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @author  WoW-CMS
+ * @copyright  Copyright (c) 2017 - 2021, WoW-CMS.
+ * @license https://opensource.org/licenses/MIT MIT License
+ * @link    https://wow-cms.com
+ * @since   Version 1.0.1
+ * @filesource
+ */
+
+class Recruit_model extends CI_Model
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->auth = $this->load->database('auth', TRUE);
+    }
+
+    public function getAccountID($id)
+    {
+        return $this->auth->select('id')->where('id', $id)->get('account')->row('id');
+    }
+
+    public function getRecruiterID($id)
+    {
+        return $this->auth->select('recruiter')->where('id', $id)->get('account')->row('recruiter');
+    }
+
+    public function setRecruiter($recruitId)
+    {
+        $existingAccount = $this->auth->select('id')->where('id', $recruitId)->get('account')->row('id');
+
+        if (!$existingAccount)
+        {
+            return 'accountIDNotFound';
+        }
+        else
+        {
+            $this->auth->set('recruiter', $recruitId)->where('id', $this->session->userdata('wow_sess_id'))->update('account');
+            return 'accountIDFound';
+        }
+    }
+}
